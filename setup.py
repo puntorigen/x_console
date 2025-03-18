4afb66afbecc561f0a6394612c5b4e0b247bdb96
Pablo Schaffner <pablo@puntorigen.com>
2025-03-18 19:01:32 -0300

    - updated README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='x_console',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     package_dir={'': 'x_console'},
     entry_points={},
