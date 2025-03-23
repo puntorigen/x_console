3bbcdecf9f76f97798d538cdc0737475e9c50d17
Pablo Schaffner <pablo@puntorigen.com>
2025-03-22 23:44:51 -0300

    - increased version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='x_console',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     package_dir={'x_console': 'x_console'},
     entry_points={},
